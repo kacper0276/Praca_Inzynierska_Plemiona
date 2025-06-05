c0e92d2e1b797c78aa59b39e1768f83221ff1528
kacper0276 <kacper0276@op.pl>
2025-06-05 08:15:37 +0200

    [FE] Create basic pipes

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -4,9 +4,16 @@ import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
 import { MatIconModule } from '@angular/material/icon';
+import { OrderByPipe } from './pipes/order-by.pipe';
+import { TimeAgoPipe } from './pipes/time-ago.pipe';
+import { TruncatePipe } from './pipes/truncate.pipe';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    OrderByPipe,
+    TimeAgoPipe,
+    TruncatePipe
+  ],
   imports: [
     CommonModule,
     HttpClientModule,
