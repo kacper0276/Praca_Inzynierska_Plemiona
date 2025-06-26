0aaa5c39995b47015741c52b7f4362dc87f63112
kacper0276 <kacper0276@op.pl>
2025-06-26 19:52:02 +0200

    [BE] Implement user module with controller, service, and gateway; add base entity and role enum; create activation email template

---
+++ backend/src/users/entities/user.entity.ts
@@ -0,0 +1,39 @@
+import { Column, Entity } from 'typeorm';
+import { BaseEntity } from '../../entities/base.entity';
+import { Role } from '../../enums/role.enum';
+
+@Entity({ name: 'users' })
+export class User extends BaseEntity {
+  @Column({ nullable: false })
+  email: string;
+
+  @Column({ nullable: false })
+  login: string;
+
+  @Column({ nullable: true })
+  firstName: string;
+
+  @Column({ nullable: true })
+  lastName: string;
+
+  @Column({ nullable: false })
+  password: string;
+
+  @Column({ type: 'enum', enum: Role, default: Role.USER })
+  role: Role;
+
+  @Column({ type: 'longtext', nullable: true })
+  profileImage: string;
+
+  @Column({ type: 'longtext', nullable: true })
+  backgroundImage: string;
+
+  @Column({ default: false })
+  isActive: boolean;
+
+  @Column({ default: false })
+  isOnline: boolean;
+
+  @Column({ default: '' })
+  bio: string;
+}
