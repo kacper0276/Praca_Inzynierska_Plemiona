03466dc71da8d5ce42f7c373ec6b01c0f24e3763
kacper0276 <kacper0276@op.pl>
2025-09-10 00:41:31 +0200

    [FE] Add language selection and theme toggle functionality to side menu

--- client/src/app/modules/game/components/side-menu/side-menu.component.ts
+++ client/src/app/modules/game/components/side-menu/side-menu.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { Theme } from '../../../../shared/types/theme.type';
 
 @Component({
   selector: 'app-side-menu',
@@ -8,8 +10,47 @@ import { Component, Input } from '@angular/core';
 export class SideMenuComponent {
   @Input() activeTab: string = 'village';
   isCollapsed = false;
+  currentLang = 'pl';
+  currentTheme: Theme = 'light';
+
+  constructor(private translate: TranslateService) {}
+
+  ngOnInit(): void {
+    const storedLang = localStorage.getItem('lang');
+    if (storedLang) {
+      this.setLanguage(storedLang);
+    } else {
+      this.currentLang = this.translate.getDefaultLang() || 'pl';
+      this.translate.use(this.currentLang);
+    }
+
+    const storedTheme = (localStorage.getItem('theme') as Theme) || 'light';
+    this.applyTheme(storedTheme);
+  }
 
   toggleCollapse() {
     this.isCollapsed = !this.isCollapsed;
+  }
+
+  setLanguage(lang: string) {
+    this.currentLang = lang;
+    this.translate.use(lang);
+    localStorage.setItem('lang', lang);
+  }
+
+  toggleTheme() {
+    const next: Theme = this.currentTheme === 'dark' ? 'light' : 'dark';
+    this.applyTheme(next);
+  }
+
+  private applyTheme(theme: Theme) {
+    this.currentTheme = theme;
+    const body = document.body;
+    if (theme === 'dark') {
+      body.classList.add('dark-theme');
+    } else {
+      body.classList.remove('dark-theme');
+    }
+    localStorage.setItem('theme', theme);
   }
 }
