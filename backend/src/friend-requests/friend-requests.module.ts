703821f435eafbd07cc0074521cf56958f08c54a
kacper0276 <kacper0276@op.pl>
2025-11-25 20:06:09 +0100

    [BE] Small fix

--- backend/src/friend-requests/friend-requests.module.ts
+++ backend/src/friend-requests/friend-requests.module.ts
@@ -1,12 +1,16 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { FriendRequest } from './entities/friend-request.entity';
 import { FriendRequestsRepository } from './repositories/friend-requests.repository';
 import { FriendRequestsService } from './services/friend-requests.service';
 import { FriendRequestsController } from './controllers/friend-requests.controller';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([FriendRequest])],
+  imports: [
+    TypeOrmModule.forFeature([FriendRequest]),
+    forwardRef(() => UsersModule),
+  ],
   controllers: [FriendRequestsController],
   providers: [FriendRequestsRepository, FriendRequestsService],
   exports: [FriendRequestsRepository],
