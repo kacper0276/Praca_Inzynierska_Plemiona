f7043cb55d5feecf01f2f3a1fe14bf23a54a3e9d
kacper0276 <kacper0276@op.pl>
2025-12-11 10:37:59 +0100

    [FE] Fix in admin panel styles

--- client/src/app/modules/admin-panel/components/building-panel/building-panel.component.ts
+++ client/src/app/modules/admin-panel/components/building-panel/building-panel.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BuildingsService } from '@modules/game/services/buildings.service';
+import { ColumnDefinition } from '@shared/interfaces/column-definition.interface';
+import { BuildingData } from '@shared/models';
 
 @Component({
   selector: 'app-building-panel',
   templateUrl: './building-panel.component.html',
-  styleUrl: './building-panel.component.scss'
+  styleUrl: './building-panel.component.scss',
 })
-export class BuildingPanelComponent {
+export class BuildingPanelComponent implements OnInit {
+  buildingsList: BuildingData[] = [];
+  buildingColumns: ColumnDefinition[] = [
+    {
+      key: 'name',
+      header: 'Nazwa budynku',
+    },
+    {
+      key: 'level',
+      header: 'Poziom budynku',
+    },
+    {
+      key: 'health',
+      header: 'Poziom zdrowia',
+    },
+    {
+      key: 'userLogin',
+      header: 'Nazwa użytkownika',
+    },
+    {
+      key: 'userVillageId',
+      header: 'Id wioski użytkownika',
+    },
+  ];
+  isModalOpen: boolean = false;
+  selectedBuilding: BuildingData | null = null;
 
+  constructor(private readonly buildingsService: BuildingsService) {}
+
+  ngOnInit(): void {
+    this.fetchAllBuildings();
+  }
+
+  private fetchAllBuildings() {
+    this.buildingsService.getAllBuildings().subscribe({
+      next: (res) => {
+        console.log(res);
+        this.buildingsList = res.data;
+      },
+    });
+  }
 }
--- client/src/app/modules/game/services/buildings.service.ts
+++ client/src/app/modules/game/services/buildings.service.ts
@@ -1,7 +1,13 @@
 import { Injectable } from '@angular/core';
+import { ApiResponse, BuildingData } from '@shared/models';
 import { HttpService } from '@shared/services/http.service';
+import { Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class BuildingsService {
   constructor(private readonly httpService: HttpService) {}
+
+  getAllBuildings(): Observable<ApiResponse<BuildingData[]>> {
+    return this.httpService.get<BuildingData[]>(`/buildings`);
+  }
 }
