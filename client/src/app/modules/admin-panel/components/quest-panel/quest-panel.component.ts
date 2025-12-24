edf36e7ddac10e49eb184b11d59cee1f0d17f76e
kacper0276 <kacper0276@op.pl>
2025-12-24 15:12:10 +0100

    [FE] Add QuestPanel component and integrate quest management routes in admin panel

---
+++ client/src/app/modules/admin-panel/components/quest-panel/quest-panel.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quest-panel',
+  templateUrl: './quest-panel.component.html',
+  styleUrl: './quest-panel.component.scss'
+})
+export class QuestPanelComponent {
+
+}
--- client/src/app/modules/game/services/quests.service.ts
+++ client/src/app/modules/game/services/quests.service.ts
@@ -1,7 +1,31 @@
 import { Injectable } from '@angular/core';
+import { ApiResponse, UserQuestProgress } from '@shared/models';
 import { HttpService } from '@shared/services';
+import { Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class QuestsService {
   constructor(private readonly httpService: HttpService) {}
+
+  getTasksForServer(
+    serverId: number
+  ): Observable<ApiResponse<UserQuestProgress[]>> {
+    return this.httpService.get<UserQuestProgress[]>(
+      `/quests/server/${serverId}`
+    );
+  }
+
+  startTaskOnServer(
+    questId: number,
+    serverId: number
+  ): Observable<ApiResponse<UserQuestProgress>> {
+    return this.httpService.post<UserQuestProgress>(
+      `/quests/${questId}/start/${serverId}`,
+      {}
+    );
+  }
+
+  changeObjectiveProgress() {}
+
+  createQuest() {}
 }
