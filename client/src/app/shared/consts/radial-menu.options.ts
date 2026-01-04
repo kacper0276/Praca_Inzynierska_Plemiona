7307df5f31802bc3837e5929f282870ed718db07
kacper0276 <kacper0276@op.pl>
2026-01-04 21:52:31 +0100

    [FE] Add new icon

--- client/src/app/shared/consts/radial-menu.options.ts
+++ client/src/app/shared/consts/radial-menu.options.ts
@@ -3,13 +3,11 @@ import { RadialMenuOption } from '../models';
 export const EMPTY_PLOT_OPTIONS: RadialMenuOption[] = [
   {
     action: 'build',
-    // iconName: 'build',
     iconUrl: '/assets/icons/key.png',
     tooltip: 'Zbuduj nowy budynek',
   },
   {
     action: 'inspect',
-    // iconName: 'search',
     iconUrl: '/assets/icons/pencil.png',
     tooltip: 'Informacje o polu',
   },
@@ -23,18 +21,16 @@ export const BUILDING_OPTIONS: RadialMenuOption[] = [
   },
   {
     action: 'upgrade',
-    iconName: 'upgrade',
+    iconUrl: '/assets/icons/building_upgrade.png',
     tooltip: 'Rozbuduj',
   },
   {
     action: 'destroy',
-    // iconName: 'delete',
     iconUrl: '/assets/icons/trash.png',
     tooltip: 'Usuń',
   },
   {
     action: 'edit',
-    // iconName: 'edit',
     iconUrl: '/assets/icons/pencil.png',
     tooltip: 'Edytuj',
   },
